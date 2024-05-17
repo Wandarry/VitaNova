aaa5c2859bd4fee37cf1c2ccc8d84801c6faeba3
Enick <berryenick20@gmail.com>
2024-05-17 19:08:47 +0200

    chore: Storybook integration
    Storybook integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { PropsWithChildren, useEffect } from "react";
+import "react-native-reanimated";
+import Constants from "expo-constants";
+import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed"
+import { config } from "@gluestack-ui/config" // Optional if you want to use default theme
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+function Wrapper({ children }: PropsWithChildren) {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -27,11 +34,39 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <GluestackUIProvider config={config}>
+        {children}
+      </GluestackUIProvider>
     </ThemeProvider>
   );
 }
+
+const AppContent = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+};
+
+// Default to rendering your app
+let AppEntryPoint = () => (
+  <Wrapper>
+    <AppContent />
+  </Wrapper>
+);
+
+// Render Storybook if storybookEnabled is true
+if (Constants.expoConfig?.extra?.storybookEnabled === "true") {
+  const Storybook = require("../.storybook").default;
+  AppEntryPoint = () => (
+    <Wrapper>
+      <Stack.Screen></Stack.Screen>
+      <Storybook />
+    </Wrapper>
+  );
+}
+
+export default AppEntryPoint;
