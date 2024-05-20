1d3e117dc04d2901a110b83f50782e539e7a5716
Enick <berryenick20@gmail.com>
2024-05-20 18:22:31 +0200

    chore: Add design system text input
    Add design system text input

---
+++ .storybook/stories/TextInput.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { TextInput } from '../../components/uikit/Input/TextInput';
+//import { MailIcon } from '@gluestack-ui/themed';
+
+const TextInputMeta: Meta<typeof TextInput> = {
+    title: 'Uikit/Input/TextInput',
+    component: TextInput,
+    args: {
+        placeholder: 'Enter text...',
+        //icon: MailIcon,
+    },
+  };
+
+  export default TextInputMeta;
+
+export const Basic: StoryObj<typeof TextInput> = {};
+
+
+
+
+
+
