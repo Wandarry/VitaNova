bab22a6a8f00a0be80d6b839793939555dbc522f
Enick <berryenick20@gmail.com>
2024-05-22 04:08:59 +0200

    chore: Add design system icons
    Add design system icons

---
+++ components/icons/arrowOut.tsx
@@ -0,0 +1,14 @@
+import { createIcon } from "@gluestack-ui/themed";
+import { Path } from "react-native-svg";
+
+export const ArrowOut = createIcon({
+  viewBox: "0 0 16 16",
+  path: (
+    <>
+      <Path
+        d="M4.26671 12L3.33337 11.0667L9.73337 4.66666H4.00004V3.33333H12V11.3333H10.6667V5.6L4.26671 12Z"
+        fill="currentColor"
+      />
+    </>
+  ),
+});
