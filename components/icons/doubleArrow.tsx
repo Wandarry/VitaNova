6bc0c7b8ede2418ecb032ab7b298abee574146f0
Enick <berryenick20@gmail.com>
2024-05-22 04:08:59 +0200

    chore: Add design system icons
    Add design system icons

---
+++ components/icons/doubleArrow.tsx
@@ -0,0 +1,14 @@
+import { createIcon } from "@gluestack-ui/themed";
+import { Path } from "react-native-svg";
+
+export const DoubleArrow = createIcon({
+  viewBox: "0 0 25 24",
+  path: (
+    <>
+      <Path
+        d="M10.075 12L5.5 7.4L6.9 6L12.9 12L6.9 18L5.5 16.6L10.075 12ZM16.675 12L12.1 7.4L13.5 6L19.5 12L13.5 18L12.1 16.6L16.675 12Z"
+        fill="currentColor"
+      />
+    </>
+  ),
+});
---
+++ components/icons/east.tsx
@@ -0,0 +1,15 @@
+import { createIcon } from "@gluestack-ui/themed";
+import { Path } from "react-native-svg";
+
+export const East = createIcon({
+  viewBox: "0 0 16 16",
+  path: (
+    <>
+      <Path
+        d="M10.0002 12.6667L9.05016 11.7333L12.1168 8.66668H1.3335V7.33334H12.1168L9.06683 4.26668L10.0002 3.33334L14.6668 8.00001L10.0002 12.6667Z"
+        fill="currentColor"
+      />
+    </>
+  ),
+});
+
