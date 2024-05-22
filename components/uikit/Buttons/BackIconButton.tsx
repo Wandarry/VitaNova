3d0700fa1277db3cd324245fea5b837ffddc7a96
Enick <berryenick20@gmail.com>
2024-05-22 16:52:00 +0200

    chore: Add back icon button
    Add back icon button

---
+++ components/uikit/Buttons/BackIconButton.tsx
@@ -0,0 +1,37 @@
+import { Button, ButtonIcon } from "@gluestack-ui/themed";
+import React from "react";
+import { ArrowLeft } from "@/components/icons/arrowLeft";
+
+export type BackIconButtonProps = {
+    isDisabled: boolean,
+    bgColor?: string,
+    pressedBgColor?: string,
+    iconColor?: string,
+    onPress?: () => void;
+}
+
+export const BackIconButton = ({
+  isDisabled,
+  bgColor="$primaryLight",
+  pressedBgColor="$primaryLightHover",
+  iconColor="$primaryNormal",
+  onPress,
+}:BackIconButtonProps) => {
+  return (
+    <Button
+      borderRadius="$full"
+      p={4}
+      w={42}
+      bg={bgColor}
+      pressed-bgColor={pressedBgColor}
+      onPress={onPress}
+      isDisabled={isDisabled}
+      $disabled-opacity="$50"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <ButtonIcon as={ArrowLeft} color={iconColor} size="xl"/>
+    </Button>
+  );
+};
+
