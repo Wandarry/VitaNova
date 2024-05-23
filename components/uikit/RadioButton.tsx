0f870fde6298f64cec71ac03e58d662c379e69ad
Enick <berryenick20@gmail.com>
2024-05-23 12:06:09 +0200

    chore: Add radio button
    Add radio button

---
+++ components/uikit/RadioButton.tsx
@@ -0,0 +1,33 @@
+import {
+  CircleIcon,
+  Radio,
+  RadioGroup,
+  RadioIcon,
+  RadioIndicator,
+  RadioLabel,
+} from "@gluestack-ui/themed";
+
+export type RadioButtonProps = {
+  label: string;
+  value: string;
+  onChange: () => void;
+  isDisabled: boolean;
+};
+
+export const RadioButton = ({
+  value,
+  onChange,
+  isDisabled,
+  label,
+}: RadioButtonProps) => {
+  return (
+    <RadioGroup>
+      <Radio value={value} isDisabled={isDisabled} onChange={onChange} $disabled-opacity="$50">
+        <RadioIndicator mr="$3" borderColor="$primaryNormal">
+          <RadioIcon as={CircleIcon} color="$primaryNormal"/>
+        </RadioIndicator>
+        <RadioLabel fontSize="$lg" color="$black">{label}</RadioLabel>
+      </Radio>
+    </RadioGroup>
+  );
+};
