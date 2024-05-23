60afea81ce75d31ba377b7c844bc05679c855d52
Enick <berryenick20@gmail.com>
2024-05-23 12:06:09 +0200

    chore: Add radio button
    Add radio button

---
+++ .storybook/stories/RadioButton.stories.tsx
@@ -0,0 +1,25 @@
+import { View } from 'react-native';
+import type { Meta, StoryObj } from '@storybook/react';
+import { RadioButton } from '../../components/uikit/RadioButton';
+import React from 'react';
+
+const RadioButtonMeta: Meta<typeof RadioButton> = {
+  title: 'uikit/Input/RadioButton',
+  component: RadioButton,
+  args: {
+    label: "Tous les organes et tissus nécessaires",
+    isDisabled: false,
+  },
+  decorators: [
+    (Story) => (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <Story />
+      </View>
+    ),
+  ],
+};
+
+export default RadioButtonMeta;
+
+export const Basic: StoryObj<typeof RadioButton> = {};
+
