710b47a8949fbe2cadf6e6caeabfdba9706ed1f2
Enick <berryenick20@gmail.com>
2024-05-29 22:25:56 +0200

    chore: Create flatlist compoent for news and events
    Create flatlist compoent for news and events

---
+++ components/Events.tsx
@@ -0,0 +1,55 @@
+import { Box, HStack, Text } from "@gluestack-ui/themed";
+import { LinkButton } from "./uikit/Buttons/LinkButton";
+import { NewsEventsSlider } from "./newsEventsSlider";
+
+const data = [
+  {
+    id: "1",
+    image: require("@/assets/images/news1.png"),
+    title: "Pourquoi le don d'organes est-il cruciale de nos jours ?",
+  },
+  {
+    id: "2",
+    image: require("@/assets/images/news2.png"),
+    title: "Pourquoi le don d'organes est-il cruciale de nos jours ?",
+  },
+  {
+    id: "3",
+    image: require("@/assets/images/news3.png"),
+    title: "Pourquoi le don d'organes est-il cruciale de nos jours ?",
+  },
+  {
+    id: "4",
+    image: require("@/assets/images/news1.png"),
+    title: "Pourquoi le don d'organes est-il cruciale de nos jours ?",
+  },
+  {
+    id: "5",
+    image: require("@/assets/images/news2.png"),
+    title: "Pourquoi le don d'organes est-il cruciale de nos jours ?",
+  },
+  {
+    id: "6",
+    image: require("@/assets/images/news3.png"),
+    title: "Pourquoi le don d'organes est-il cruciale de nos jours ?",
+  },
+];
+
+export function Events() {
+  return (
+    <Box gap="$4">
+      <HStack justifyContent="space-between" alignItems="center">
+        <Text fontSize={21} fontWeight="$semibold">
+          Événements
+        </Text>
+        <LinkButton
+          title="Tout voir"
+          withIcon={true}
+          isDisabled={false}
+          onpress={() => console.log("Link button pressed")}
+        />
+      </HStack>
+      <NewsEventsSlider data={data} />
+    </Box>
+  );
+}
---
+++ components/newsEventsItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { VStack, Image, Text } from "@gluestack-ui/themed";
+
+type ItemProps = {
+  title: string;
+  image: string; // Use any for the image source
+};
+
+export const NewsEventsItem = ({ title, image }: ItemProps) => {
+  return (
+    <VStack gap={8} w={180} marginRight={18}>
+      <Image source={image} h={180} w={180} borderRadius={8} alt="VitaNova" />
+      <Text fontWeight="$semibold">{title}</Text>
+    </VStack>
+  );
+};
