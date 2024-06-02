77d1010e44c5f84e390ad3d3adf44262ebf3a27c
Enick <berryenick20@gmail.com>
2024-06-02 16:17:19 +0200

    chore: Create user collection class
    Create user collection class

---
+++ firebase/collections/user.ts
@@ -0,0 +1,25 @@
+import { UserEntity } from "@/types/collections/user";
+import { BaseCollection } from "./BaseCollections";
+import { FirebaseCollectionsNames } from "../collectionsNames";
+
+let instance: UserCollection;
+
+class UserCollection extends BaseCollection<UserEntity> {
+  constructor() {
+    if (instance) {
+      throw new Error("You can only create one instance!");
+    }
+    // import FirebaseCollectionsNames please
+    super(FirebaseCollectionsNames.users);
+    instance = this;
+  }
+
+  // obtain a user information record from the user's e-mail address
+  getUserByEmail(email: string): Promise<UserEntity | null> {
+    return this.getFirstBy("email", email);
+  }
+}
+
+const userCollectionInstance = Object.freeze(new UserCollection());
+
+export default userCollectionInstance;
