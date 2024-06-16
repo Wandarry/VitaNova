500c6bb9fdc7769b6cb52211f6c0727a295be684
Enick <berryenick20@gmail.com>
2024-06-16 23:24:33 +0200

    chore: Create article fetching hooks
    Create article fetching hooks

---
+++ hooks/useMostRecentArticle.ts
@@ -0,0 +1,8 @@
+import articleCollectionInstance from "@/firebase/collections/ArticleCollection";
+import { useFetch } from "./useFetch";
+
+export const useMostRecentArticle = () => {
+  const state = useFetch(articleCollectionInstance.getMostRecentArticle);
+
+  return state;
+};
