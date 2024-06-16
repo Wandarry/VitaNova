937219e2eb32327309de15b46c4ef63792ae1d81
Enick <berryenick20@gmail.com>
2024-06-16 23:16:51 +0200

    chore: Extend base collection class
    Extend base collection class

--- firebase/collections/BaseCollections.ts
+++ firebase/collections/BaseCollections.ts
@@ -11,6 +11,10 @@ import {
   query,
   getDocs,
   DocumentData,
+  orderBy,
+  updateDoc,
+  increment,
+  UpdateData,
 } from "firebase/firestore";
 import firestoreDb from "../firestore";
 
@@ -41,5 +45,33 @@ export class BaseCollection<T extends DocumentData> {
     const querySnapshot = await getDocs<T, T>(q);
 
     return querySnapshot.docs[0]?.data() ?? null;
+  }
+
+  async getAllMostRecent(limitSize: number) {
+    const ref = collection(firestoreDb, this.name) as CollectionReference<T, T>;
+
+    const orderByQuery = orderBy("createdAt", "desc");
+    const limitQuery = limit(limitSize);
+
+    const q = query<T, T>(ref, orderByQuery, limitQuery);
+    const querySnapshot = await getDocs<T, T>(q);
+
+    return querySnapshot.docs.map((doc) => doc.data());
+  }
+  async getAllBy(fieldName: string, value: string) {
+    const ref = collection(firestoreDb, this.name) as CollectionReference<T, T>;
+
+    const q = query<T, T>(ref, where(fieldName, "==", value), limit(1));
+    const querySnapshot = await getDocs<T, T>(q);
+
+    return querySnapshot.docs.map((doc) => doc.data());
+  }
+
+  async incrementFieldValue(id: string, fieldName: keyof T, value: number) {
+    const ref = doc(firestoreDb, this.name, id) as DocumentReference<T, T>;
+
+    return updateDoc<T, T>(ref, {
+      [fieldName]: increment(value),
+    } as UpdateData<T>);
   }
 }
